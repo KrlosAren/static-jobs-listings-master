8b83051ebca19ec2edced907dfe7fdc03456aa7f
Carlos Lopez <krlosaren@gmail.com>
2020-07-25 21:12:17 -0400

    styles almost ready

--- src/app.js
+++ src/app.js
@@ -2,3 +2,24 @@
 // window.addEventListener('load', render)
 
 import './styles/sass/main.scss'
+
+
+const containerFilter = document.querySelector('.filter')
+
+const clearButton = document.getElementById('clear')
+
+const filter = Array.from(document.querySelectorAll('#filter')  )
+filter.forEach(category =>{
+  category.addEventListener('click', () =>{
+    const filter = []
+    containerFilter.style.display = 'flex'
+    const sessionStorage = window.sessionStorage
+    filter.push(category.textContent)
+    sessionStorage.setItem('filter', JSON.stringify(filter))
+  })
+
+
+  clearButton.addEventListener('click', () => {
+    containerFilter.style.display = 'none'
+  })
+})
