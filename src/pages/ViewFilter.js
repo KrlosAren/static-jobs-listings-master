54bec626c1b24946d1acdd0ac73f544305d15254
Carlos Lopez <krlosaren@gmail.com>
2020-07-26 23:37:30 -0400

    js in the way

--- src/pages/ViewFilter.js
+++ src/pages/ViewFilter.js
@@ -1,6 +1,10 @@
-function ViewFilter() {
+function ViewFilter(filter) {
+  debugger
   const view = `
     <div class="filter">
+      ${filter.map( section =>`
+        <span>${section}</span>
+      `).join('')}
       <h3 class="clear">Clear</h3>
     </div>
   `;
--- src/utils/Filter.js
+++ src/utils/Filter.js
@@ -1,11 +1,15 @@
 import ViewFilter from '../pages/ViewFilter'
+import FilterContent from '../pages/FilterContent'
 
 const Filter = () => {
   const filterContent = document.getElementById('filterContent')
   const filter = Array.from(document.querySelectorAll('#filter'))
   filter.map(btn => {
     btn.addEventListener('click', () => {
-      filterContent.innerHTML = ViewFilter()
+      const filters = []
+      const filteres = FilterContent(btn)
+      filters.push(filteres)
+      filterContent.innerHTML = ViewFilter(filters)
     })
   })
 }
