1c4199b4f3363db7b044f0ce2ba902f22b4ff602
Carlos Lopez <krlosaren@gmail.com>
2020-08-13 21:19:54 -0400

    add design files

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,19 @@ import '../styles/sass/main.scss'
 import data from './db.json'
 import Job from '../pages/Job'
 
+import myhome from '../assets/myhome.svg'
+import manage from '../assets/manage.svg'
+import insure from '../assets/insure.svg'
+import faceit from '../assets/faceit.svg'
+import shortly from '../assets/shortly.svg'
+import account from '../assets/account.svg'
+import photosnap from '../assets/photosnap.svg'
+import eyecamco from '../assets/eyecam-co.svg'
+import loopstudios from '../assets/loop-studios.svg'
+import theairfiltercompany from '../assets/the-air-filter-company.svg'
+
+
+
 const app = document.getElementById('app')
 
 const render = async () => {
